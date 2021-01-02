ec80f2f63b626bdaba4597564480c2e7dfa10328
Czy <czy610175932@qq.com>
2021-01-02 17:10:58 +0800

    form表单

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="hello">
+    <form>
+      <el-form label-width="80px" >
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8" :offset="0">
+            <el-form-item label="特殊资源">
+              <el-input></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+    </el-form>
+    </form>
+  </div>
+</template>
+
+<script src='./HelloWorld.js'>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
